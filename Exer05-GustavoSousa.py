b230c4932b82f9de296088180c734b4f0a3892e4
GustavoSSousaa <gustasantossousa2008@gmail.com>
2025-02-18 19:47:45 -0300

    ba

---
+++ Exer05-GustavoSousa.py
@@ -0,0 +1,5 @@
+#Peça ao usuário para colocar um numero que deseja a tabuada e em seguida exiba a tabuada desse numero
+n1 = int(input("Digite um numero que deseja saber a tabuada : "))
+for i in range(1,11):
+    print (n1,"*",i,"=",n1*i)
+    
